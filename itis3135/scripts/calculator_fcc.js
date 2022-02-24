c3b42978595d8cc5cbe50e934b725f9e0d17a329
evschl <eschlee@uncc.edu>
2022-02-24 16:25:54 -0500

    Update calculator_fcc.js

--- itis3135/scripts/calculator_fcc.js
+++ itis3135/scripts/calculator_fcc.js
@@ -1,5 +1,30 @@
 const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.calculator_keys');
+const display = document.querySelector('.calculator_display');
+
+const calculate = (n1, operator, n2) => 
+{
+    let result = '';
+
+    if (operator === 'add')
+    {
+        result = parseFloat(n1) + parseFloat(n2);
+    }
+    else if (operator === 'subtract')
+    {
+        result = parseFloat(n1) - parseFloat(n2);
+    }
+    else if (operator === 'multiply')
+    {
+        result = parseFloat(n1) * parseFloat(n2);
+    }
+    else if (operator === 'divide')
+    {
+        result = parseFloat(n1) / parseFloat(n2);
+    }
+
+    return result;
+}
 
 keys.addEventListener('click', e => 
 {
@@ -7,19 +32,38 @@ keys.addEventListener('click', e =>
     {
         const key = e.target;
         const action = key.dataset.action;
+        const keyContent = key.textContent;
+        const displayedNum = display.textContent;
 
         if (!action) 
         {
+            if (displayedNum === '0' || previousKeyType === 'operator')
+            {
+                display.textContent = keyContent;
+            } 
+            else
+            {
+                display.textContent = displayedNum + keyContent;
+            }
+
             console.log('number key!');
         }
 
         if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide')
         {
+            key.classList.add('is-depressed');
+            calculator.dataset.previousKeyType = 'operator';
+
+            calculator.dataset.firstValue = displayedNum;
+            calculator.dataset.operator = action;
+
             console.log('operator key!');
         }
 
         if (action === 'decimal')
         {
+            display.textContent = displayedNum + '.';
+            
             console.log('decimal key!');
         }
 
@@ -30,7 +74,16 @@ keys.addEventListener('click', e =>
 
         if (action === 'calculate')
         {
+            const firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            const secondValue = displayedNum;
+            
+            display.textContent = calculate(firstValue, operator, secondValue);
+
             console.log('equal key!');
         }
+
+        Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'));
+
     }
 })
