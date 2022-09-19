4fc10d7601661b8ff7e6d72921c75a4f9c620a14
evschl <eschlee@uncc.edu>
2022-09-19 13:42:39 -0400

    gesg

--- itis3135/testing/testscript.js
+++ itis3135/testing/testscript.js
@@ -2,6 +2,6 @@ alert("Welcome to my page! Give me money.");
 
 var money = 0;
 while(money < 500) {
-    money = prompt("Enter the amount of money in USD you want to give. (minimum $500");
+    money = prompt("Enter the amount of money in USD you want to give. (minimum $500)");
 }
 alert("Thank you for $" + money + "!");
