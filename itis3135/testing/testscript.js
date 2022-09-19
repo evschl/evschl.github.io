9ddf7bb77dc4b65523a2c36ef12095a289473ea8
evschl <eschlee@uncc.edu>
2022-09-19 18:17:12 -0400

    hujh

--- itis3135/testing/testscript.js
+++ itis3135/testing/testscript.js
@@ -1,8 +1,23 @@
 alert("Welcome to my page! Give me money.");
 
+//Initialized Variables
+today = new Date();
+
+/*
 var money = 0;
 
 while(money < 500) {
     money = prompt("Enter the amount of money in USD you want to give. (minimum $500)");
 }
 alert("Thank you for $" + money + "!");
+*/
+
+function displayDateTime() {
+    document.writeln("Today's Date");
+    document.writeln(today.toDateString());
+    document.writeln("<br>");
+    document.writeln("Today's Time");
+    document.write(today.getHours());
+    document.write(":");
+    document.write(today.getMinutes());
+}
