9730e079d45ee7123239194337eb5c0210bfc837
evschl <eschlee@uncc.edu>
2022-09-19 13:39:08 -0400

    testjs

---
+++ itis3135/testing/testscript.js
@@ -0,0 +1,7 @@
+alert("Welcome to my page! Give me money.");
+
+var money = 0;
+while(money < 500) {
+    money = prompt("Enter the amount of money in USD you want to give. (minimum $500");
+}
+alert("Thank you for $" + money + "!");
