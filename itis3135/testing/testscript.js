a61ee49dbbda6f009beea43ea1bb4d7d44eaa0fa
evschl <eschlee@uncc.edu>
2022-09-19 17:53:02 -0400

    bhvgh

--- itis3135/testing/testscript.js
+++ itis3135/testing/testscript.js
@@ -1,12 +1,8 @@
 alert("Welcome to my page! Give me money.");
 
 var money = 0;
-var today = new Date();
+
 while(money < 500) {
     money = prompt("Enter the amount of money in USD you want to give. (minimum $500)");
 }
 alert("Thank you for $" + money + "!");
-
-void displayDate() {
-    document.write(today.toDateString());
-};
