53257ea9c7fe5b19acf752cbb017a442d6a5a7e3
evschl <eschlee@uncc.edu>
2022-09-21 18:09:04 -0400

    bjkjnbj

--- itis3135/testing/testscript.js
+++ itis3135/testing/testscript.js
@@ -12,12 +12,47 @@ alert("Thank you for $" + money + "!");
 function displayDateTime() 
 {
     let today = new Date();
-    //let year = today.getYear();
-    //let month = today.getMonth();
-    //let date = today.getDate();
-    //let hour = today.getHours();
-    //let minute = today.getMinutes();
+    let year = today.getYear();
+    let month = today.getMonth();
+    let date = today.getDate();
+    let hour = today.getHours();
+    let minute = today.getMinutes();
     let second = today.getSeconds();
-    document.getElementById('clock').innerHTML = second;
+    let meridian = "PLACEHOLDER";
+
+    if(hour >= 13)
+    {
+        hour -= 12;
+        meridian = "PM";
+    }
+    else
+    {
+        meridian = "AM";
+    }
+
+    if(date % 10 == 1) 
+    {
+        date = date + "st";
+    } 
+    else if(date % 10 == 2) 
+    {
+        date = date + "nd";
+    }
+    else if(date % 10 == 3)
+    {
+        date = date + "rd";
+    }
+    else
+    {
+        date = date + "th";
+    }
+
+    var monthsAsText = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+ 
+    var dateText = "Today is " + monthsAsText[month + 1] + " " + date + ", " + year;
+    var clockText = hour + ":" + minute + ":" + second + ":" + meridian;
+
+    document.getElementById('dateDisplay').innerHTML = dateText;
+    document.getElementById('clockDisplay').innerHTML = clockText;
     setTimeout(displayDateTime, 1000);
 }
