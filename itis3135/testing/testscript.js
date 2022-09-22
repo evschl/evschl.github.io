3a6056202a6c9e9cf41f3d0dec228d4cd7c463ce
evschl <eschlee@uncc.edu>
2022-09-22 14:29:41 -0400

    Update testscript.js

--- itis3135/testing/testscript.js
+++ itis3135/testing/testscript.js
@@ -13,7 +13,7 @@ function displayRandomTask()
 {
     let task = new Array("Admire a squirrel", "Befriend a squirrel", "Tax Evasion", "Knit a squirrel-themed sweater (for a squirrel)", "Climb a tree");
     randNum = Math.floor(Math.random * 5);
-    document.getElementById("displayRandomTask").innerHTML = Testing;
+    document.getElementById("displayRandomTask").innerHTML = "Testing";
 }
 
 function userFeeling() 
