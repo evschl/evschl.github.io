3ef5164075434830f9928210df501a78f16bbff0
evschl <eschlee@uncc.edu>
2022-09-22 15:11:27 -0400

    Update testscript.js

--- itis3135/testing/testscript.js
+++ itis3135/testing/testscript.js
@@ -12,7 +12,7 @@ alert("Thank you for $" + money + "!");
 function displayRandomTask() 
 {
     let task = new Array("Admire a squirrel", "Befriend a squirrel", "Tax Evasion", "Knit a squirrel-themed sweater (for a squirrel)", "Climb a tree");
-    let randNum = Math.floor(Math.random * 5);
+    //let randNum = Math.floor(Math.random * 5);
     document.getElementById("displayRandomTask").innerHTML = task[0];
 }
 
