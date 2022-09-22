981234c18502c35a1da68963e9c29591e71c3ca1
evschl <eschlee@uncc.edu>
2022-09-22 15:33:53 -0400

    Update testscript.js

--- itis3135/testing/testscript.js
+++ itis3135/testing/testscript.js
@@ -25,7 +25,7 @@ function randomTask()
 {
     let task = new Array("Admire a squirrel", "Befriend a squirrel", "Tax Evasion", "Knit a squirrel-themed sweater (for a squirrel)", "Climb a tree");
     let randNum = Math.floor(Math.random * 5);
-    document.getElementById("displayRandomTask").innerHTML = task[1];
+    document.getElementById("displayRandomTask").innerHTML = task[randNum];
 }
 
 function calculateGas()
