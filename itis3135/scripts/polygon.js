363278068e93e4245908f787997b04af833e15bf
evschl <eschlee@uncc.edu>
2022-09-27 12:36:25 -0400

    Update polygon.js

--- itis3135/scripts/polygon.js
+++ itis3135/scripts/polygon.js
@@ -2,49 +2,52 @@ let polygon = 100;
 
 alert("Welcome to the Earnest Squirrels!");
 
-while(polygon < 1 || polygon > 10)
-{
-    polygon = prompt("Can't remember the name of a polygon? Enter a number (1-10), and you will be given the name of the polygon with that many sides!");
-    polygon = Math.round(Math.abs(polygon));
+function validateEntry(entry) {
+    while(entry < 1 || entry > 10) {
+        entry = validateEntry(prompt("Can't remember the name of a polygon? Enter a number (1-10), and you will be given the name of the polygon with that many sides!"));
+    }
+
+    return entry;
 }
 
-    if(polygon == 1) 
+function polygonToString(sides) {
+    if(sides == 1) 
     {
         alert("The polygon with 1 side is known as a \"Henagon\"!");
     } 
-    else if(polygon == 2)
+    else if(sides == 2)
     {
         alert("The polygon with 2 sides is known as a \"Digon\"!");
     }
-    else if(polygon == 3)
+    else if(sides == 3)
     {
         alert("The polygon with 3 sides is known as a \"Trigon\" (or Triangle)!");
     }
-    else if(polygon == 4)
+    else if(sides == 4)
     {
         alert("The polygon with 4 sides is known as a \"Tetragon\" (or Quadrilateral)!");
     }
-    else if(polygon == 5)
+    else if(sides == 5)
     {
         alert("The polygon with 5 sides is known as a \"Pentagon\"!");
     }
-    else if(polygon == 6)
+    else if(sides == 6)
     {
         alert("The polygon with 6 sides is known as a \"Hexagon\"!");
     }
-    else if(polygon == 7)
+    else if(sides == 7)
     {
         alert("The polygon with 7 sides is known as a \"Heptagon\" (or Septagon)!");
     }
-    else if(polygon == 8)
+    else if(sides == 8)
     {
         alert("The polygon with 8 sides is known as a \"Octagon\"!");
     }
-    else if(polygon == 9)
+    else if(sides == 9)
     {
         alert("The polygon with 9 sides is known as a \"Enneagon\" (or Nonagon)!");
     }
-    else if(polygon == 10)
+    else if(sides == 10)
     {
         alert("The polygon with 10 sides is known as a \"Decagon\"!");
     }
@@ -52,3 +55,9 @@ while(polygon < 1 || polygon > 10)
     {
         alert("How did you get here?");
     }
+}
+
+polygon = validateEntry(prompt("Can't remember the name of a polygon? Enter a number (1-10), and you will be given the name of the polygon with that many sides!"));
+polygon = Math.round(Math.abs(polygon));
+
+polygonToString(polygon);
