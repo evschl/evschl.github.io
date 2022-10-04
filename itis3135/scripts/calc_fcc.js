d6591a02acb20ffee79eb4a25e2883c850c7b440
evschl <eschlee@uncc.edu>
2022-10-04 13:45:52 -0400

    Update calc_fcc.js

--- itis3135/scripts/calc_fcc.js
+++ itis3135/scripts/calc_fcc.js
@@ -7,19 +7,19 @@ const calculate = (n1, operator, n2) =>
 
     if(operator === 'add')
     {
-        result = n1 + n2;
+        result = parseFloat(n1) + parseFloat(n2);
     }
     else if(operator === 'subtract')
     {
-        result = n1 - n2;
+        result = parseFloat(n1) - parseFloat(n2);
     }
     else if(operator === 'multiply')
     {
-        result = n1 * n2;
+        result = parseFloat(n1) * parseFloat(n2);
     }
     else if(operator === 'divide')
     {
-        result = n1 / n2;
+        result = parseFloat(n1) / parseFloat(n2);
     }
 
     return result;
