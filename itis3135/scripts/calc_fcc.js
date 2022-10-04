1d43c09d85026c7ab64814fd6439386924637857
evschl <eschlee@uncc.edu>
2022-10-04 13:39:16 -0400

    Update calc_fcc.js

--- itis3135/scripts/calc_fcc.js
+++ itis3135/scripts/calc_fcc.js
@@ -1,14 +1,28 @@
 const calculator = document.querySelector('.calculator');
-const keys = calculator.querySelector('.calculator_keys');
+const keys = calculator.querySelector('.calculator_keys');  
 
-keys.addEventListener('click', e => {
-    if(e.target.matches('button')) {
-        /*
+keys.addEventListener('click', e => 
+{
+    if(e.target.matches('button')) 
+    {
         const key = e.target;
         const action = key.dataset.action;
+        const keyContent = key.textContent;
+        const displayedNum = display.textContent;
+        const previousKeyType = calculator.dataset.previousKeyType;
 
-        if(!action) {
-            console.log('number key!');
+        Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'));
+
+        if(!action) 
+        {
+            if(displayedNum === '0' || previousKeyType === 'operator')
+            {
+                display.textContent = keyContent;
+            }
+            else
+            {
+                display.textContent = displayedNum + keyContent;
+            }
         }
 
         if(
@@ -16,49 +30,28 @@ keys.addEventListener('click', e => {
             action === 'subtract' ||
             action === 'multiply' ||
             action === 'divide'
-        ) {
-            console.log('operator key!');
+        ) 
+        {
+            key.classList.add('is-depressed');
+            calculator.dataset.previousKeyType = 'operator';
+            
+            calculator.dataset.firstValue = displayedNum;
+            calculator.dataset.operator = action;
         }
 
-        if(action === 'decimal') {
-            console.log('decimal key!');
+        if(action === 'decimal') 
+        {
+            display.textContent = displayedNum + '.';
         }
 
-        if(action === 'clear') {
+        if(action === 'clear') 
+        {
             console.log('clear key!');
         }
 
-        if(action === 'calculate') {
-            console.log('equal key!');
+        if(action === 'calculate') 
+        {
+            const secondValue = displayedNum;
         }
-        */
     }
 })
-
-const key = e.target;
-const action = key.dataset.action;
-
-if(!action) {
-    console.log('number key!');
-}
-
-if(
-    action === 'add' ||
-    action === 'subtract' ||
-    action === 'multiply' ||
-    action === 'divide'
-) {
-    console.log('operator key!');
-}
-
-if(action === 'decimal') {
-    console.log('decimal key!');
-}
-
-if(action === 'clear') {
-    console.log('clear key!');
-}
-
-if(action === 'calculate') {
-    console.log('equal key!');
-}
