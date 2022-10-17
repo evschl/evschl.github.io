908ee7be58132061c42bda9ed87a15bafce81cb1
evschl <eschlee@uncc.edu>
2022-10-17 19:04:35 -0400

    h

---
+++ itis3135/scripts/arrays.js
@@ -0,0 +1,18 @@
+alert("Script is working!");
+
+const person = ["Chas", "Luna", "Lucas", "Evan"];
+const salary = [10000, 11000, 12000, 13000];
+
+function addSalary(salary) 
+{
+
+}
+
+function displayResults() {
+    
+}
+
+function displaySalary(salary) 
+{
+    
+}
