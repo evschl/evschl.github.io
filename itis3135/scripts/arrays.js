0e64a7757136a87d087b92f98b5e8763f69888e7
evschl <eschlee@uncc.edu>
2022-10-19 12:25:50 -0400

    hi

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -1,11 +1,12 @@
 alert("Script is working!");
 
-const person = ["Sol Horthman", "Ness Parcheesi", "Chase Batta", "Wagyu Pearlman", "Spencer Schlee", "Mohammed Boyd", "Stefanie Burnett", "Maisy Prudent"];
-const salary = [50000, 75000, 50000, 65000, 85000, 100000, 100000, 75000];
+const person = [];
+const salary = [];
 
-function addSalary(salary) 
+function addSalary(newEmployee, newSalary) 
 {
-
+    person.push(newEmployee);
+    salary.push(newSalary);
 }
 
 function displayResults() 
@@ -23,6 +24,7 @@ function displayResults()
     var avg = total / salary.length;
     document.getElementById("average").innerHTML = avg;
     document.getElementById("highest").innerHTML = highest;
+    return 0;
 }
 
 function displaySalary() 
