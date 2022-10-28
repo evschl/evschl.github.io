90a366fa2fd0ef62aa3896b6338688a56e7af462
evschl <eschlee@uncc.edu>
2022-10-28 14:27:32 -0400

    jjejejjaga

---
+++ itis3135/components/scripts/slides.js
@@ -0,0 +1,10 @@
+let animSpeed = 1000;
+let pause = 3000;
+let interval;
+
+function startSlideshow()
+{
+    interval = setInterval(function(){}, pause);
+}
+
+//LEFT OFF AT: Inside our function we will use jQuery to fade between slides at the speed of the animationSpeed variable:
