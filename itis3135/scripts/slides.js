c3d1f1fbca8815e5311a83470160b37ce01ca20b
evschl <eschlee@uncc.edu>
2022-11-03 14:14:53 -0400

    Update slides.js

--- itis3135/scripts/slides.js
+++ itis3135/scripts/slides.js
@@ -5,11 +5,12 @@ let interval;
 function startSlideshow()
 {
     interval = setInterval(function(){}, pause);
-    $('.slides > li:first')
-    .fadeOut(animSpeed)
-    .next()
-    .fadeIn(animSpeed)
-    .end()
-    .appendTo('.slides');
 }
+
+$('.slides > li:first')
+.fadeOut(animSpeed)
+.next()
+.fadeIn(animSpeed)
+.end()
+.appendTo('.slides');
 //LEFT OFF AT: Inside our function we will use jQuery to fade between slides at the speed of the animationSpeed variable:
