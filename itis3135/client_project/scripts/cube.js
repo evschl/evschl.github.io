b3bed71e61e20b58ee3db70a0c69c00611fdd9c8
evschl <eschlee@uncc.edu>
2022-11-10 15:11:03 -0500

    hehe

---
+++ itis3135/client_project/scripts/cube.js
@@ -0,0 +1,27 @@
+function display(archeId)
+{
+    archeArray = document.getElementsByClassName("displayArchetypeInfo");
+    for(let i in archeArray)
+    {
+        if(archeArray[i].id == archeId)
+        {
+            archeArray[i].style.display = "block";
+            let hideBtn = archeId + 100;
+            document.getElementById(hideBtn).innerHTML.style.display = "none";
+        }
+    }
+}
+
+function hide(btnId)
+{
+    archeArray = document.getElementsByClassName("displayArchetypeInfo");
+    for(let i in archeArray)
+    {
+        if(archeArray[i].id == btnId)
+        {
+            archeArray[i].style.display = "none";
+            let hideBtn = btnId + 100;
+            document.getElementById(hideBtn).innerHTML.style.display = "block";
+        }
+    }
+}
