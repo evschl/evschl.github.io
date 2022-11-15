0df6922166597c3f51329bdfa51a823f4e664cda
evschl <eschlee@uncc.edu>
2022-11-15 12:44:57 -0500

    ff

--- itis3135/scripts/imgSwap.js
+++ itis3135/scripts/imgSwap.js
@@ -1,21 +1,28 @@
 $(document).ready(function() {
 
         // preload the image for each link
-        $("#image_list li").click(
-                function() 
-                {
-                        $(this).toggleClass("minus");
-                        $(this).next().slideToggle(1000);
-                }
-        );
+        $("#image_list a").each(function() {
+                var swappedImage = new Image();
+                swappedImage.src = $(this).attr("href");
+        });
 
         // set up the event handlers for each link
+        $("#image_list a").click(function(evt) {
+                $("#main_image").fadeOut(1000);
+                $("#caption").fadeOut(1000);
 
-    			// get the image URL and caption for each image and animate the caption
+                var imageURL = $(this).attr("href");
+                $("#main_image").attr("src", imageURL);
+        
+                var caption = $(this).attr("title");
+                $("#caption").text(caption);
 
-            // cancel the default action of each link
+                $("#main_image").fadeIn(1000);
+                $("#caption").fadeIn(1000);
 
-
+                // cancel the default action of each link
+                evt.preventDefault();
+        });
     // move the focus to the first link
-
+    $("li:first-child a").focus();
 }); // end ready
