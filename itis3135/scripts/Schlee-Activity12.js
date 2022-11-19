6330e624a3b2f1a87107ba53eecaebde6bce0b5e
evschl <eschlee@uncc.edu>
2022-11-19 12:58:08 -0500

    m

--- itis3135/scripts/Schlee-Activity12.js
+++ itis3135/scripts/Schlee-Activity12.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
     $("#slider").bxSlider({
         auto: true,
-        minSlides: 2,
-        maxSlides: 2,
-        slideWidth: 300,
-        slideMargin: 20
+        randomStart: true,
+        captions: true,
+        pager: true,
+        pagerSelector: "#id_pager",
+        minSlides: 1,
+        maxSlides: 1,
+        slideWidth: 500,
+        slideMargin: 20,
+        speed: 3000
     });
 });
